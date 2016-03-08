6505d64b845a0117c501586310663645cd216cd1
sylwia211 <sylwia_karbowiak@op.pl>
2016-03-08 10:43:39 +0100

    Basics for Lexer.

---
+++ Lexer.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include "include/Lexer.h"
+using std::cout;
+using std::cin;
+using std::endl;
+
+using std::string;
+using std::getline;
+
+
+using namespace std;
+Lexer::Lexer()
+{
+    //ctor
+}
+
+Lexer::~Lexer()
+{
+    //dtor
+}
+
+string Lexer::lecture(const string& fileName) {
+
+    std::ifstream t(fileName.c_str());
+    if (t.is_open())
+    {
+    t.seekg(0, std::ios::end);
+    size_t size = t.tellg();
+    std::string lines(size, ' ');
+    t.seekg(0);
+    t.read(&lines[0], size);
+        return lines;
+    }
+    else cout << "Unable to open file";
+        exit(-1);
+        return NULL;
+}
+
+string Lexer::parseToSymbols(const string& example) {
+    //std::string example = lines;
+    std::string symbols;
+    std::string word;
+
+    // Notice how you can loop through a string just like a vector<char>
+    for(size_t i = 0; i < example.size(); ++i) {
+        char c = example[i];
+
+        // When we see whitespace, print the current word and clear it
+        if(c == ' ' || c == '\t' || c == '\n') {
+            // Don't print anything if we don't have a word
+            if(!word.empty()) {
+                std::cout << word << std::endl;
+                word.clear();
+            }
+        } else if(c=='=' || c==',' || c==';' || c=='+' || c==':') {
+            if(!word.empty()) {
+                std::cout << word << std::endl;
+                word.clear();
+            }
+                word = c;
+                if(c == ':') {
+                    word += example[++i];
+                }
+                std::cout << word << std::endl;
+                word.clear();
+
+        } else {
+            // Append the current character to the end of the string
+            word += c; // or word.push_back(c)
+        }
+    }
+    // In case the line doesn't end with whitespace
+    if(!word.empty()) {
+        std::cout << word << std::endl;
+    }
+
+
+
+
+    return symbols;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include "include/Lexer.h"
+using std::cout;
+using std::cin;
+using std::endl;
+
+using std::string;
+using std::getline;
+
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
-    return 0;
+
+    string fileName = "C:/Users/Sylwia/Documents/ProjetLG/test.txt";
+    Lexer* lex = new Lexer();
+    string names = lex->lecture(fileName);
+    string examples = lex->parseToSymbols(names);
+
+    //print array
+    //int len = names.length();
+    //for(int i = 0; i < len; i++)
+    //{
+    //     cout<<names[i];
+    //}
+
+
+     return 0;
 }
