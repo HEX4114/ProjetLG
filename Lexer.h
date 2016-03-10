5cf14a83e0157994403f827b04c784f8f969dcd7
sylwia211 <sylwia_karbowiak@op.pl>
2016-03-10 11:28:03 +0100

    Code refactoring, still to develop.

--- Lexer.h
+++ Lexer.h
@@ -1,20 +1,37 @@
 #ifndef LEXER_H
 #define LEXER_H
 #include <string>
+#include <vector>
+#include "Symbole/Symbole.h"
 using std::string;
 
+#include "Symbole\Phrase\Declaration\DeclarationConstante.h"
+#include "Symbole\Phrase\Declaration\DeclarationVariable.h"
+#include "Symbole\Phrase\Instruction\Affectation.h"
 
 class Lexer
 {
     public:
         Lexer();
         virtual ~Lexer();
-        string lecture(const std::string& fileName);
-        string parseToSymbols(const std::string& examples);
-        //Symbole getNext();
+        string lecture(string& fileName);
+        void parseToSymbols(string& examples);
+        Symbole getNext();
+        void addSymbole(string& word)
+		{
+			symboles.push_back(word);
+		}
+		string getSymbole(int next)
+		{
+			return symboles.at(next);
+		}
+
     protected:
 
     private:
+        //void addNonEmptyWord(string& word);
+        std::vector<string> symboles;
+        static int next;
 };
 
 #endif // LEXER_H
