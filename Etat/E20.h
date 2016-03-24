e6b54c6cfdb7910258d666ffead089c828516ea3
fcallet <francois.callet@insa-lyon.fr>
2016-03-24 20:00:31 +0100

    one last compilation problem

--- Etat/E20.h
+++ Etat/E20.h
@@ -10,7 +10,7 @@ class E20 : public Etat
 public:
 	E20();
 	virtual ~E20();
-	virtual bool transition(Automate& automate, Symbole* symbole) throw(std::pair<int, std::string>);
+	virtual bool transition(Automate& automate, Symbole* symbole);
 protected:
 	std::string name;
 private:
